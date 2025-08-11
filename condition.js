e318cb101816b129432062a5c1222ecd3a208b9a
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-11 23:18:23 +0600

    variable discribed

---
+++ condition.js
@@ -0,0 +1,6 @@
+//let's recape our previous concepts:
+//! variable :
+//a variable is like a container where we can store many types of data ;
+// we declair variable using the keywords: var let const ex:
+var name = "maskur";
+var age = 24;
