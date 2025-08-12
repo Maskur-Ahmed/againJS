e4c3225a3166a6c589f627b9e0538f374f130dc6
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-12 22:51:26 +0600

    to do added

---
+++ function.js
@@ -0,0 +1,10 @@
+function myFunc (){
+
+}
+let arr = ["maskur",]
+
+arr.push("ahmed");
+//who to create a object .update a object ,delete a object;
+
+//:step 1:get user input value from dom element
+//ToDo:loop 
