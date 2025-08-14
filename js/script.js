0f57d5126c5dfbe18bfeceb57a1fadd41ff42e77
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-14 08:11:33 +0600

    let cleared

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let massage = 'hello';
+massage = 'hi';
+massage = 'world';
+
+let age = 22;
+age = 21;
+age = 20;
+
+let user = 'maskur';
+user = 'ahmed';
+user = 'Maskur';
+
+let userName = "Maskur";
+userName = 'Ahmed';
+let lastName = userName;
+alert(lastName);
