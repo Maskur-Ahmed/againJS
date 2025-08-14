23ff420b4da2b64b8fce8193ae85fd14073b24cb
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-15 01:48:00 +0600

    all selectors added

--- js/dom.js
+++ js/dom.js
@@ -23,6 +23,7 @@ let btn2 = document.querySelector('.btn2');
 let btn3 = document.querySelector('.btn3');
 let btn4 = document.querySelector('.btn4');
 
+
 btn1.addEventListener('click',()=>{
     div.style = 'background-color:red';
     div.innerText = 'mui lal rong';
