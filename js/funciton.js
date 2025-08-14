b5e8a3801166848d81f887ca368c13a6671c6f63
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-15 00:47:19 +0600

    function cleared

---
+++ js/funciton.js
@@ -0,0 +1,20 @@
+
+// function kire (){
+//     console.log("oi kire ");
+// }
+
+// kire();
+
+
+let myFunc = function(){
+    console.log("hello sir !");
+    
+}
+myFunc();
+ 
+let arr = ()=>{
+    console.log("hi there");
+
+}
+
+arr();
--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ let age = prompt("tmr boyos koto?");
 
 if (age >= 40) {
   alert("Bura hoye gesen!");
+
 }else if(age >= 20){
     alert("aso baba bosho");
+
     let salary = prompt("mashe koto kamao?");
   if (salary >= 50000) {
     alert("darao amr meye k ani!");
+
   }else{
     alert("eto kom takay hobena baba");
   }
