edcaf8fcd7a18da2ca918334d7b3f1f195a91eb2
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-15 01:21:41 +0600

    1st dom task done

---
+++ js/dom.js
@@ -0,0 +1,15 @@
+let kire = document.querySelector('.ki');
+let btnOne = document.querySelector('#btn');
+let h2 = document.querySelector('h2');
+let btntwo = document.querySelector('#btn2');
+
+btnOne.addEventListener('click',()=>{
+    
+    kire.style = 'color:green';
+})
+
+
+btntwo.addEventListener('click',()=>{
+    h2.innerText = 'modhu modhu modhu';
+    h2.style = "color:red"
+})
