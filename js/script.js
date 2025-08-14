26dc8f17adb63e2f8955d8ebcf2546e04f7a79d7
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-14 11:25:29 +0600

    class 2 complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-//! comparision :
-let x = "6000";
+let age = prompt("tmr boyos koto?");
 
-console.log(x <= 600)
+if (age >= 40) {
+  alert("Bura hoye gesen!");
+}else if(age >= 20){
+    alert("aso baba bosho");
+    let salary = prompt("mashe koto kamao?");
+  if (salary >= 50000) {
+    alert("darao amr meye k ani!");
+  }else{
+    alert("eto kom takay hobena baba");
+  }
+}
+ else {
+  alert("tor boyos hoyni");
+}
