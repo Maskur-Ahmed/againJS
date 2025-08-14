48ce27e3912e4ea8f7060d724caf21819642e79e
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-15 01:45:27 +0600

    create div on html

--- js/dom.js
+++ js/dom.js
@@ -12,5 +12,33 @@ btnOne.addEventListener('click',()=>{
 btntwo.addEventListener('click',()=>{
     h2.innerText = 'modhu modhu modhu';
     h2.style = "color:red"
-    
+
+})
+
+//----------------- task 3 ----------------------
+
+let div = document.querySelector('div');
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+let btn3 = document.querySelector('.btn3');
+let btn4 = document.querySelector('.btn4');
+
+btn1.addEventListener('click',()=>{
+    div.style = 'background-color:red';
+    div.innerText = 'mui lal rong';
+})
+
+btn2.addEventListener('click',()=>{
+    div.style = 'background-color:green';
+    div.innerText = 'mui sobuj rong';
+})
+
+btn3.addEventListener('click',()=>{
+    div.style = 'background-color:blue';
+    div.innerText = 'mui nill rong';
+})
+
+btn4.addEventListener('click',()=>{
+    div.style = 'background-color:yellow';
+    div.innerText = 'mui holud rong';
 })
