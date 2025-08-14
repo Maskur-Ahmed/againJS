dd7572ed58e206a3a50999155067c2af71fe6eed
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-14 08:19:02 +0600

    const cleard

--- js/script.js
+++ js/script.js
@@ -13,4 +13,11 @@ user = 'Maskur';
 let userName = "Maskur";
 userName = 'Ahmed';
 let lastName = userName;
-alert(lastName);
+
+//*const:
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+let color = COLOR_GREEN
+alert(color);
