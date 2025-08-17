3d33fe1e15c676a015646493b0c6616d957986ad
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-17 21:58:55 +0600

    mach function ready

--- js/funciton.js
+++ js/funciton.js
@@ -5,4 +5,24 @@ function myFunc (){
 
 }
 
-myFunc()
+// myFunc()
+
+function sum (a,b){
+    let result =console.log(a + b);
+    return result;
+
+}
+
+sum(10,40)
+
+function macherTorkari (mach,sobji,mosla){
+    console.log('tel gorom kor');
+    console.log('moshla de');
+    console.log('shaksobji de');
+    console.log(mach + ' de');
+    return `${mach} macher torkari ready`;
+}
+
+let torkari = macherTorkari("ilish","alu","holud");
+
+console.log(torkari)
