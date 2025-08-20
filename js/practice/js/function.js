822ce4cc933002216ca006dc98b1a634df41c431
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-20 14:49:28 +0600

    task two complete

--- js/practice/js/function.js
+++ js/practice/js/function.js
@@ -4,4 +4,21 @@ function Multi (one,two,three,four){
 }
 
 let result = Multi(27,39,20,38);
-console.log(result);
+// console.log(result);
+//--------------------------task one complete--------------------------;
+//===================================================================================
+function mulDev (number){
+    if(number % 2 === 0){
+        let even = number / 2;
+        return even;
+    }else{
+        return number * 2;
+    }
+}
+let check = 5;
+let res = mulDev(check);
+
+console.log(res);
+
+//------------------------------------task two complete------------------------------------;
+//========================================================================================
